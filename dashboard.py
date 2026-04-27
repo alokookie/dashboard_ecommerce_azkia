ffa3e824e34e420439ff28dbbbe6079800da7479
alokookie <109732235+alokookie@users.noreply.github.com>
2026-04-27 23:34:05 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,15 +1,21 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 st.title("Dashboard Analisis E-Commerce")
 
 # load data dari csv
 top_3 = pd.read_csv('top_3.csv')
 bottom_3 = pd.read_csv('bottom_3.csv')
-avg_items = pd.read_csv('avg_items.csv')['avg_items'][0]
+
+avg_items = pd.read_csv('avg_items.csv')['avg_items'].iloc[0]
+
 avg_monthly = pd.read_csv('avg_monthly.csv')
+
+# clean data
+avg_monthly['month'] = avg_monthly['month'].astype(str)
+avg_monthly['price'] = pd.to_numeric(avg_monthly['price'], errors='coerce')
+avg_monthly = avg_monthly.dropna(subset=['price'])
 
 # Kategori produk yang sering dan paling jarang dibeli customer
 st.subheader("Kategori Produk")
@@ -24,17 +30,19 @@ with col2:
     st.write("Top 3 Kategori Paling Jarang Dibeli")
     st.dataframe(bottom_3)
 
-# Rata-rata jumlah produk dalam satu pesanan
-
+#Rata-rata jumlah produk dalam satu pesanan
 st.subheader("Rata-rata Produk per Order")
 st.metric("Rata-rata", f"{avg_items:.2f}")
 
-# Tren Transaksi Bulana
-
+# tren tranksaksi bulanan 
 st.subheader("Tren Transaksi Bulanan")
 
-fig, ax = plt.subplots(figsize=(10,5))
-sns.lineplot(data=avg_monthly, x='month', y='price', ax=ax)
-plt.xticks(rotation=45)
+fig, ax = plt.subplots(figsize=(10, 5))
+
+ax.plot(avg_monthly['month'], avg_monthly['price'], marker='o')
+
+ax.set_xlabel("Month")
+ax.set_ylabel("Price")
+ax.tick_params(axis='x', rotation=45)
 
 st.pyplot(fig)
